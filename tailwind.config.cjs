d394feafee445bb3dcaa5b513e0445d9e2426270
Llavrov <levlevlavrov@gmail.com>
2023-04-10 16:19:23 +0300

    add PeopleEatChoice and tailwind setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -101,6 +101,9 @@ const config = {
             fontFamily: {
                 manrope: ['Manrope', 'Arial', 'sans-serif'],
             },
+            fontFamily: {
+                manrope: ['Manrope'],
+            },
         },
     },
     plugins: [],
